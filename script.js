943ac05579d188aee070a78da39a71e77638256c
ElaineCSOliveira <83356291+ElaineCSOliveira@users.noreply.github.com>
2026-06-21 13:51:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Interação simples para os botões
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert("Obrigado por participar do App Solidário!");
+  });
+});
